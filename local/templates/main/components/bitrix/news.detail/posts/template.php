97b0da4c6511dfe3f06dff8746baf957295612c4
Shakhov Evgeniy <79115839983@ya.ru>
2021-10-13 11:28:31 +0300

    add detail news

---
+++ local/templates/main/components/bitrix/news.detail/posts/template.php
@@ -0,0 +1,14 @@
+<?php if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
+
+<!-- Post Content -->
+<article>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-10 mx-auto">
+                <?=$arResult['DETAIL_TEXT'];?>
+            </div>
+        </div>
+    </div>
+</article>
+
+<hr>
